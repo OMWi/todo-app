fcc4c5017b4a2b23beabcb885fcc37bb006f17e8
OMWi <bsuiromwi@gmail.com>
2022-05-19 14:38:35 +0300

    upd

--- angular-todoapp/src/app/app.module.ts
+++ angular-todoapp/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-todoapp/src/environments/environment.ts
+++ angular-todoapp/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'itirod-data',
+    appId: '1:354056821546:web:1783859c7d24d505d75c53',
+    databaseURL: 'https://itirod-data-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'itirod-data.appspot.com',
+    apiKey: 'AIzaSyBW9Vf6F3f2EnZY9JS9ZitWXu07Im3WX1I',
+    authDomain: 'itirod-data.firebaseapp.com',
+    messagingSenderId: '354056821546',
+    measurementId: 'G-SVSJGPNVQK',
+  }
 };
 
 /*
